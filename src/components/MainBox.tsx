c0cdd156012b3d22bc6e873cb533cf09b3ff99c9
lleull <leulmekonnen049@gmail.com>
2024-10-13 13:46:16 +0300

    Suggest to you

--- src/components/MainBox.tsx
+++ src/components/MainBox.tsx
@@ -5,7 +5,6 @@ const MainBox = ({ children }: any) => {
     return (
         <div className='w-screen h-[100vh] flex flex-row  justify-center overflow-scroll  relative  scrollbar-hide'>
             <div className='w-[65vh] border-custom-border bg-custom-light border-2 rounded-t-3xl border-b-0 mt-5   items-center justify-center  scrollbar-hide'>
-                <SuggestionPeople />
 
                 {children}
             </div>
