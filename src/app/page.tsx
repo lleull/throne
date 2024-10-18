70061e10664e842cb69eb93dd21670c4e3f0f289
lleull <leulmekonnen049@gmail.com>
2024-10-18 08:35:06 +0300

    Images Animation added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,26 +7,23 @@ import img3 from "@/assets/p3.jpeg"
 import img4 from "@/assets/pd.jpeg"
 import img5 from "@/assets/pddd.jpeg"
 import img6 from "@/assets/pdd.jpeg"
-
-
-import { useSelector, UseSelector } from 'react-redux'
 import Image from 'next/image'
-interface Hometype {
-  CustomPost: any
-}
-const Home: React.FC<Hometype> = ({ children }: any) => {
-  const [isModalOpen, setModalOpen] = useState(false);
+
+const Home = () => {
 
   return (
-    <div className='list'>
-      <Image src={img1} className='image' alt='hello' />
-      <Image src={img2} className='image' alt='hello' />
-      <Image src={img3} className='image' alt='hello' />
-      <Image src={img4} className='image' alt='hello' />
-      <Image src={img5} className='image' alt='hello' />
-      <Image src={img6} className='image' alt='hello' />
-      {/* <Image src={img1/} className='image' alt='hello' /> */}
+    <div className='flex flex-col items-center w-full'>
+      <h2 className='text-6xl font-serif '>WELCOME TO MY ALBUM</h2>
+      <div className='list'>
+        <Image src={img1} className='image' alt='hello' />
+        <Image src={img2} className='image' alt='hello' />
+        <Image src={img3} className='image' alt='hello' />
+        <Image src={img4} className='image' alt='hello' />
+        <Image src={img5} className='image' alt='hello' />
+        <Image src={img6} className='image' alt='hello' />
+        {/* <Image src={img1/} className='image' alt='hello' /> */}
 
+      </div>
     </div>
   )
 }
