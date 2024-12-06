7a9b86d755c9768502c6bdf1b349d3ee4b1fd46a
lleull <leulmekonnen049@gmail.com>
2024-12-06 13:32:24 +0300

    Throne Starter

--- src/app/component/main-bar.tsx
+++ src/app/component/main-bar.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Main_bar = () => {
     return (
-        <div>Main_bar</div>
+        <div className='w-full flex items-start justify-center mt-10 h-screen'>
+            <h2 className='font-serif text-5xl text-white'>
+                Chat AI
+            </h2>
+        </div>
     )
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        custom: ['YourFontName', 'sans-serif'], // Use a fallback font as well
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
