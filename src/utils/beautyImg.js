580bf165e35fa5bc39010afa215f2de2b95abe1d
lleull <leulmekonnen049@gmail.com>
2024-12-06 14:06:47 +0300

    Images addded

---
+++ src/utils/beautyImg.js
@@ -0,0 +1,38 @@
+import pic1 from "../assets/pic1.jpg"
+import pic2 from "../assets/pic2.jpg"
+import pic3 from "../assets/pic3.jpg"
+import pic4 from "../assets/pic4.jpg"
+import pic5 from "../assets/pic5.jpg"
+import pic6 from "../assets/pic6.jpg"
+import pic7 from "../assets/pic7.jpg"
+import pic8 from "../assets/pic8.jpg"
+import pic9 from "../assets/pic9.jpg"
+
+export const BeautyImages = [
+    {
+        id: 1,
+        image: pic1
+    },
+    {
+        id: 2,
+        image: pic2
+    }, {
+        id: 3,
+        image: pic3
+    }, {
+        id: 4,
+        image: pic4
+    }, {
+        id: 5,
+        image: pic5
+    }, {
+        id: 6,
+        image: pic6
+    }, {
+        id: 7,
+        image: pic7
+    }, {
+        id: 8,
+        image: pic8
+    },
+]
