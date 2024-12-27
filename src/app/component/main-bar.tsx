2cd339e863d5fea0235a5641f6b7217df4319927
lleull <leulmekonnen049@gmail.com>
2024-12-27 12:05:11 +0300

    Axios added

--- src/app/component/main-bar.tsx
+++ src/app/component/main-bar.tsx
@@ -13,39 +13,38 @@ const Main_bar = () => {
                 setnextImage(nextImage + 1)
 
             }
-
-
         }, 2);
-        console.log("NUMMM", nextImage)
+
+
+
     }, [nextImage])
+
     return (
         <div className='flex flex-col  items-center   h-screen'>
             <div className='w-full h-24 bg-gray-800 flex items-center justify-between pl-20'>
-                <h1 className='text-3xl'>
+                <h1 className='text-3xl text-rose-50'>
 
-                    Make Up
+                    Make Up Courses
                 </h1>
                 <div className='flex-2 gap-6 flex pr-20'>
 
                     {
-                        [{
-                            name: "decorations"
-                        },
-                        {
-                            name: "MakeUps"
-                        }, {
-                            name: "HumanHair"
-                        }].map((items, i) => {
-                            return (
-                                <button className={`pl-5 pr-5 pt-2 pb-2 rounded-xl  text-white font-bold ${i % 2 == 0 ? `bg-gray-400 text-black` : `bg-red-200`} hover:bg-slate-200 hover:text-black cursor-pointer`} key={i}>
-                                    {items.name}
-                                </button>
-                            )
-                        })
+                        [
+                            {
+                                name: "Login"
+                            }, {
+                                name: "Register"
+                            }].map((items, i) => {
+                                return (
+                                    <button className={`pl-5 pr-5 pt-2 pb-2 rounded-xl  text-white font-bold ${i % 2 == 0 ? `bg-gray-400 text-black` : `bg-red-200`} hover:bg-slate-200 hover:text-black cursor-pointer`} key={i}>
+                                        {items.name}
+                                    </button>
+                                )
+                            })
                     }
                 </div>
             </div>
-            <div className='flex flex-row items-center  h-[50vh] overflow-hidden'>
+            <div className='flex flex-row items-center  h-[70vh] overflow-hidden'>
 
                 <p className='w-1/2 text-xl text-white text-left p-20'>
                     <strong className='font-serif text-3xl'>
@@ -55,11 +54,11 @@ const Main_bar = () => {
                     Join a community of like-minded artists and get the skills you need to excel in the beauty industry. From bridal makeup to special effects, our curriculum is designed to give you the versatility and knowledge to succeed. Unlock your full potential and start your career as a professional makeup artist today!
                 </p>
 
-                <Image className='w-1/2 rounded-l-full ' src={BeautyImages[nextImage].image} alt="s" />
+                <Image className='w-1/2 rounded-l-full' src={BeautyImages[nextImage].image} alt="s" />
 
             </div>
-            <button className='bg-slate-50 text-2xl pt-7 pb-7 pl-10 pr-10 rounded-3xl mt-24'>
-                Sign Up
+            <button className='bg-slate-50 text-2xl pt-7 pb-7 pl-3 pr-3 rounded-3xl mt-1'>
+                Explore more
             </button>
 
         </div>
