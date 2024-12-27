39f1e25e7d7a6ffe3ec9243de56881fe454a9ca6
lleull <leulmekonnen049@gmail.com>
2024-12-27 13:16:46 +0300

    Search by name

--- src/app/component/main-bar.tsx
+++ src/app/component/main-bar.tsx
@@ -5,44 +5,49 @@ import Image from 'next/image'
 import axios from 'axios'
 const Main_bar = () => {
     const [nextImage, setnextImage] = useState(1)
-    const [markertData, setMarketData] = useState()
+    // const [n]
+    const [Text, setText] = useState<string>()
 
-    const options = {
-        method: "GET",
-        mode: "nocors",
-        url: "https://coinranking1.p.rapidapi.com/coins",
-        params: {
-            referenceCurrencyUuid: "yhjMzLPhuIDl",
-            timePeriod: "24h",
-            "tiers[0]": "1",
-            orderBy: "marketCap",
-            orderDirection: "desc",
-            limit: "50",
-            offset: "0",
+    const array = [
+        {
+            id: 1,
+            name: "leul"
         },
-        headers: {
-            "X-RapidAPI-Key": "36e79fec5bmsh0641910c952cbeep1afb1ejsn132bcc2ba3cc",
-            "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+        {
+            id: 2,
+            name: "mekonnen"
         },
-    };
+        {
+            id: 3,
+            name: "abel"
+        },
+    ]
 
-    useEffect(() => {
-        axios.request(options).then((response) => {
-            console.log("response: " + response)
-        })
-        setTimeout(() => {
-            if (BeautyImages.length = nextImage) {
+    const SearchForUser = (Text) => {
 
-                setnextImage(1)
-            } else {
-                setnextImage(nextImage + 1)
 
+        return new Promise((resolve, reject) => {
+            try {
+
+                const finddname = array.find((data) => data.name === Text)
+                if (finddname) {
+                    resolve("Finded name:" + finddname.name)
+                } else {
+                    resolve("No names founds")
+
+                }
+            } catch (error) {
+                reject("No names founds")
             }
-        }, 2);
 
 
+        })
+    }
+    const handleSearch = () => {
 
-    }, [nextImage])
+        console.log(SearchForUser(Text))
+    }
+
 
     return (
         <div className='flex flex-col  items-center   h-screen'>
@@ -77,14 +82,19 @@ const Main_bar = () => {
                     </strong>  we believe that makeup is not just an art; it’s a powerful way to express creativity, boost confidence, and create beauty. Whether you're a beginner or a seasoned professional, our comprehensive makeup courses offer something for everyone. Our expert instructors provide hands-on training in everything from basic techniques to advanced makeup artistry, using the latest trends and tools in the industry.
 
                     Join a community of like-minded artists and get the skills you need to excel in the beauty industry. From bridal makeup to special effects, our curriculum is designed to give you the versatility and knowledge to succeed. Unlock your full potential and start your career as a professional makeup artist today!
+                    <div className='flex flex-col items-center justify-center'>
+
+                        <input type="text" onChange={((e) => setText(e.target.value))} placeholder='Search' className='border-none rounded pt-2 pb-2 pr-1 pl-1 w-[50%] mb-10 mt-5 text-black' />
+                        <button onClick={() => handleSearch()} className='bg-slate-50 text-xl text-black pt-7 pb-7 pl-3 pr-3 rounded-3xl mt-1 w-60'>
+
+                            Explore more
+                        </button>
+                    </div>
                 </p>
 
                 <Image className='w-1/2 rounded-l-full' src={BeautyImages[nextImage].image} alt="s" />
 
             </div>
-            <button className='bg-slate-50 text-2xl pt-7 pb-7 pl-3 pr-3 rounded-3xl mt-1'>
-                Explore more
-            </button>
 
         </div>
     )
